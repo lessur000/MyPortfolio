02bb831c17108740643895984cafb7da914a9087
lessur000 <144553293+lessur000@users.noreply.github.com>
2025-04-21 20:05:11 +0800

    Another New Project Added

--- Frontend/src/Components/sections/Project.jsx
+++ Frontend/src/Components/sections/Project.jsx
@@ -4,6 +4,8 @@ import FourthButton from "../ui/FourthButton";
 import Pic1 from "/Ecommerce.png";
 import Pic2 from "/MetaVerse.png";
 import Pic3 from "/AutomotiveShop.png";
+import Pic4 from "/RusselFlix.png";
+import FiveButton from "../ui/FiveButton";
 
 const Project = () => {
   return (
@@ -103,6 +105,38 @@ const Project = () => {
           <p className="text-gray-300 text-sm md:text-base mb-4 mt-5 font-semibold max-w-[500px]">
             Built using modern tools like Tailwind, Vite, and Node.js, Zustand.
             This ecommerce UI is responsive and fast.
+          </p>
+        </div>
+      </div>
+      {/* RusselFlix */}
+      <div className="border border-[#00BFFF] p-2 rounded-xl space-y-5 overflow-hidden ">
+        <div className="space-y-5 m-3">
+          <h5 className="text-[#A020F0] uppercase text-xl">
+            03 - RusselFlix
+          </h5>
+          <div className="flex flex-wrap gap-2">
+            <div
+              className="border bg-[#001F33] h-fit p-2 text-xs md:text-lg uppercase text-[#00BFFF]  hover:bg-[#3a005c] hover:text-white hover:border-white 
+             transition duration-300"
+            >
+              TailwindCSS
+            </div>
+            <div
+              className="border bg-[#001F33] h-fit p-2 text-xs md:text-lg uppercase text-[#00BFFF]  hover:bg-[#3a005c] hover:text-white hover:border-white 
+             transition duration-300"
+            >
+              ReactJSX+Vite
+            </div>
+          </div>
+          <FiveButton />
+          <img
+            src={Pic4}
+            alt="RusselFlix"
+            className="h-auto w-full opacity-25 hover:opacity-125 transition duration-300"
+          />
+          <p className="text-gray-300 text-sm md:text-base mb-4 mt-5 font-semibold max-w-[500px]">
+            Built using modern tools like Tailwind, Vite, Zustand.
+           
           </p>
         </div>
       </div>
